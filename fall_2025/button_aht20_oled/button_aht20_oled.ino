62717443866f1ec4d9dd582fcf582378728db53c
Nathan Eloe <powerofazure@gmail.com>
2025-09-25 13:44:55 -0500

    Add OLED to button/temp example

--- fall_2025/button_aht20_oled/button_aht20_oled.ino
+++ fall_2025/button_aht20_oled/button_aht20_oled.ino
@@ -1,21 +1,48 @@
 // Install the Adafruit AHTX0 library
 #include <Adafruit_AHTX0.h>
+#include <Adafruit_SSD1306.h>
+#include <Wire.h>
+//#include <FastLED.h>
 
 #define BTN_PIN 7
 
+//CRGB led[1];
+
+// I2C redefinitions
+#define SDA_PIN 6
+#define SCL_PIN 9
+#define LED_PIN 8
+
+TwoWire i2c(0);
 Adafruit_AHTX0 aht20;
+Adafruit_SSD1306 oled (128, 64, &i2c);
 sensors_event_t temp, humid;
 
 void setup() {
   // put your setup code here, to run once:
+  /*FastLED.addLeds<NEOPIXEL, LED_PIN>(led, 1);
+  led[0] = CRGB::Black;
+  FastLED.show();*/
   pinMode(BTN_PIN, INPUT_PULLDOWN);
   Serial.begin(115200);
   while(!Serial) delay(1);
-  if (!aht20.begin())
+  i2c.begin(SDA_PIN, SCL_PIN);
+  if (!aht20.begin(&i2c))
   {
     Serial.println("Could not init sensor. halp");
     while(1);
   }
+  if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3c))
+  {
+    Serial.println("Can't find screen");
+    while(1);
+  }
+  oled.clearDisplay();
+  oled.setTextSize(1);
+  oled.setTextColor(SSD1306_WHITE);
+  oled.setCursor(0,0);
+  oled.println(F("Hello world!"));
+  oled.display();
 
 }
 
@@ -29,6 +56,13 @@ void loop() {
     Serial.println(humid.relative_humidity);
     Serial.print("Temperature: ");
     Serial.println(temp.temperature);
+    oled.clearDisplay();
+    oled.setCursor(0,0);
+    oled.print("Humidity: "); oled.print(humid.relative_humidity); oled.println("%");
+    oled.print("Temperature: "); 
+    oled.print(temp.temperature * 9 / 5 + 32); 
+    oled.println("F");
+    oled.display();
   }
   delay(500);
 }
